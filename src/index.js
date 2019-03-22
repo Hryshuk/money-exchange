8c0e2e84a54899191e792293446544de13418951
Dmitry Hryshuk <hryshuk@tut.by>
2019-03-22 12:31:47 +0300

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -2,4 +2,28 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+
+    const coins = new Map([
+        ["H", 50],
+        ["Q", 25],
+        ["D", 10],
+        ["N", 5],
+        ["P", 1]
+    ]);
+    let object = {};
+
+    if (currency > 10000) {
+        object.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return object;
+    }
+    if (currency > 0) {
+        coins.forEach ((value, key) => {
+            let count = Math.floor(currency / value);
+            if (count) {
+                object[key] = count;
+                currency = currency % value;
+            }
+        });
+    }
+    return object;
 }
